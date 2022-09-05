5f2594d8f023b4810be29fd5fbecc71ce142a827
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-09-05 14:08:56 +0200

    🔧 prepare add_lead_contributor_role script for production

--- scripts/add_lead_contributor_role_to_feeder.sh
+++ scripts/add_lead_contributor_role_to_feeder.sh
@@ -3,9 +3,9 @@
 ### CONSTANTS
 
 # Old Feeder account
-FEEDER=0x05313c775f9dad3963786dd16710dc26de7da82ad68c6c65ef6a1062445e4321
+FEEDER=0x06ec9a30f0b9ffec60fca0ed83a3c550c3f616d2311130b008e42f454e23e61d
 
-CONTRIBUTIONS_ADDRESS=0x04aa3b2b258388a58ed429795ab56a9cd9613152755ec317f5c6bee2294e2264
+CONTRIBUTIONS_ADDRESS=0x011d60f34d8e7b674833d86aa85afbe234baad95ae6ca3d9cb5c4bcd164b7358
 
 
 ### FUNCTIONS
@@ -68,9 +68,26 @@ do
 done
 
 
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 521676353 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 204005121 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 522927651 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 499061005 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 499081602 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 508773198 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 444220622 $FEEDER"
 send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 493591124 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 501233690 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 476401778 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 481932781 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 420209573 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 486660536 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 480776993 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 493552406 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 510361047 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 418545583 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 510292638 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 457388175 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 510616270 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 511068225 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 412532737 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 504661472 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 499061005 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 498695724 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 455638351 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 510105950 $FEEDER"
+send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 420197620 $FEEDER"
