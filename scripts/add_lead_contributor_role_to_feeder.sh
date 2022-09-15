713cd7e9028478d06e052230457c677309b89c81
Anthony Buisset <abuisset@gmail.com>
2022-09-15 17:11:29 +0200

    :hammer: improve script to grant lead contributor role on projects

--- scripts/add_lead_contributor_role_to_feeder.sh
+++ scripts/add_lead_contributor_role_to_feeder.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-### CONSTANTS
-
-# Old Feeder account
-FEEDER=0x06ec9a30f0b9ffec60fca0ed83a3c550c3f616d2311130b008e42f454e23e61d
-
-CONTRIBUTIONS_ADDRESS=0x011d60f34d8e7b674833d86aa85afbe234baad95ae6ca3d9cb5c4bcd164b7358
-
-
 ### FUNCTIONS
 SCRIPT_DIR=`readlink -f $0 | xargs dirname`
 . $SCRIPT_DIR/logging.sh # Logging utilities
@@ -55,39 +47,30 @@ send_transaction() {
     echo $tx_hash
 }
 
+usage() {
+    echo "$0 -a <ACCOUNT> -c <CONTRIBUTIONS_ADDRESS> -w <LEAD_CONTRIBUTOR_WALLET> -p <PROJECT1,PROJECT2,...>"
+}
 
 ### ARGUMENT PARSING
-while getopts a:p:h option
+while getopts a:c:w:p:h option
 do
     case "${option}"
     in
         a) ACCOUNT=${OPTARG};;
+        c) CONTRACT=${OPTARG};;
+        w) WALLET=${OPTARG};;
+        p) PROJECTS=${OPTARG};;
         h) usage; exit_success;;
         \?) usage; exit_error;;
     esac
 done
 
+[ -z $ACCOUNT ] && exit_error "-a option is mandatory"
+[ -z $CONTRACT ] && exit_error "-c option is mandatory"
+[ -z $WALLET ] && exit_error "-w option is mandatory"
+[ -z $PROJECTS ] && exit_error "-p option is mandatory"
 
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 508773198 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 444220622 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 493591124 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 501233690 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 476401778 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 481932781 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 420209573 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 486660536 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 480776993 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 493552406 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 510361047 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 418545583 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 510292638 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 457388175 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 510616270 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 511068225 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 412532737 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 504661472 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 499061005 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 498695724 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 455638351 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 510105950 $FEEDER"
-send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRIBUTIONS_ADDRESS --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs 420197620 $FEEDER"
+for project in `echo $PROJECTS | tr , ' '`
+do
+    send_transaction "starknet invoke --account $ACCOUNT --network alpha-goerli --address $CONTRACT --abi build/contributions_abi.json --function add_lead_contributor_for_project --inputs $project $WALLET"
+done
